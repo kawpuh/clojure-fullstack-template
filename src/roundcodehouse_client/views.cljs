5d1e1b5c2aec38df4cfce2563953755e723064bf
kawpuh <e.r.sorrell@gmail.com>
2025-07-04 03:59:41 -0500

    simplify

--- src/roundcodehouse_client/views.cljs
+++ src/roundcodehouse_client/views.cljs
@@ -1,15 +1,6 @@
-(ns roundcodehouse-client.views
-  (:require
-   [roundcodehouse-client.card :refer [playing-card]]))
+(ns roundcodehouse-client.views)
 
 (defn main-panel []
-  [:div {:class "flex flex-wrap gap-4 p-4 bg-green-700"} ; Simulate a table
-   [playing-card :hearts :ace]
-   [playing-card :diamonds 10]
-   [playing-card :spades :queen]
-   [playing-card :clubs 7]
-   [playing-card :hearts :king]
-   ;; Example of an invalid card (shows defaults)
-   ; [playing-card :foo 99]
-   ])
+  [:div
+   "Welcome to a new page"])
 
